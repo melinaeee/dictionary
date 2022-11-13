dded0ab5c1fb597b84e558d7544bda34d1e224da
melinaeee <86803532+melinaeee@users.noreply.github.com>
2022-11-13 17:36:52 -0300

    First steps

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logologo.png';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
