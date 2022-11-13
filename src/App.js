7559aafa69d2f671efde4f4095371fb786db7166
melinaeee <86803532+melinaeee@users.noreply.github.com>
2022-11-13 18:54:16 -0300

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logologo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,6 +8,12 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Melina</small>
+      </footer>
     </div>
   );
 }
