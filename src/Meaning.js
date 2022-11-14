5751069f3cd9a141ab7eccbb8b452556596d6fe7
melinaeee <86803532+melinaeee@users.noreply.github.com>
2022-11-13 23:55:52 -0300

    synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     console.log(props.meaning);
@@ -9,13 +10,17 @@ export default function Meaning(props) {
         {props.meaning.definitions.map(function (definition, index)
         {
             return (
-                <div key={index}>
+              <div key={index}>
                 <p>
-                    {definition.definition}
-                    <br />
-                    <em>{definition.example}</em>
+                  <strong>Definition:</strong> {definition.definition}
+                  <br />
+                  <strong>Example:</strong>
+                  <em>{definition.example}</em>
+    
+                  <Synonyms synonyms={definition.synonyms} />
+                  
                 </p>
-                </div>
+              </div>
             );
         })}
       </div>
