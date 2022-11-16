99f85c0f2ea0964707f4d59f4add0e2e42957168
melinaeee <86803532+melinaeee@users.noreply.github.com>
2022-11-16 18:52:32 -0300

    added photos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import logo from './dictlogo.png';
-import './App.css';
+import logo from "./dictlogo.png";
+import "./App.css";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="img-fluid" alt="logo" />
-      </header>
-      <main>
-        <Dictionary />
-      </main>
-      <footer className="App-footer">
-        <small>Coded by Melina</small>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="dog" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Melina</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
